a2f0ed00f3122167e92e201f337c01383820ddca
SIA <Diegoamezaga@gmail.com>
2025-06-01 19:03:31 -0300

    se crean plantilla index y base para extender a vistas comunes, se modifica el modelo para que funcione atraves de componentes, se adapta el cfg de django para que opere con static. archivos css pasado a static. login maneja su propio stylesheet en el body

--- djangoback/tp3_crud/apps/juego/urls.py
+++ djangoback/tp3_crud/apps/juego/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.JuegoView.as_view(), name='juego')
+    path('juego', views.JuegoView.as_view(), name='juego'),
+    path('', views.LoginView.as_view(), name='login'),
+    path('genero', views.GeneroView.as_view(), name='genero')
 ]
--- djangoback/tp3_crud/apps/juego/views.py
+++ djangoback/tp3_crud/apps/juego/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class JuegoView(TemplateView):
-    name = "index"
-    template_name = "index.html"
+    name = "home"
+    template_name = "home.html"
+
+class LoginView(TemplateView):
+    name = "login"
+    template_name = "login.html"
+
+class GeneroView(TemplateView):
+    name = "genero"
+    template_name = "genero.html"
