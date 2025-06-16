ac7acf98f33a0ac2d025618120badab9232172ac
SIA <Diegoamezaga@gmail.com>
2025-06-16 19:44:37 -0300

    fix menor

--- djangoback/tp3_crud/apps/juego/urls.py
+++ djangoback/tp3_crud/apps/juego/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("juego", views.JuegoView.as_view(), name="juego"),
+    path("home", views.JuegoView.as_view(), name="home"),
     path("", views.LoginAuth, name="login"),
     path("register", views.register_view, name="register"),
     path("genero", views.GeneroView.as_view(), name="genero"),
