c6e25f1087675e8ff4c6bcf65cdeed3283d90153
SIA <Diegoamezaga@gmail.com>
2025-06-19 15:44:58 -0300

    se corrijió un bug que no permitia mostrar vista logeado al no tener declarada la ruta de deslogeo, se utilizó el embebido de django para el logout

--- djangoback/tp3_crud/apps/juego/urls.py
+++ djangoback/tp3_crud/apps/juego/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path("plataforma", views.PlataformaView.as_view(), name="plataforma"),
     path("decada", views.DecadaView.as_view(), name="plataforma"),
     path('juego/<int:juego_id>/', views.detalle_juego, name='detalle_juego'),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]
