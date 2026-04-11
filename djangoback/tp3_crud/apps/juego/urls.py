346b76db91061a3996e06b8c77170f2f098f2d0d
Ceci <78119165+Ceci-123@users.noreply.github.com>
2026-04-11 11:11:46 -0300

    agregado icono para chat y pop up dialogo

--- djangoback/tp3_crud/apps/juego/urls.py
+++ djangoback/tp3_crud/apps/juego/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("register", views.register_view, name="register"),
     path("genero", views.GeneroView.as_view(), name="genero"),
     path("plataforma", views.PlataformaView.as_view(), name="plataforma"),
-    path("decada", views.DecadaView.as_view(), name="plataforma"),
+    path("decada", views.DecadaView.as_view(), name="decada"),
     path('juego/<int:juego_id>/', views.detalle_juego, name='detalle_juego'),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]
