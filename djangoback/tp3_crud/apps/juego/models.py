f22fa3b6f6b5a966dadee775cc8bfc8fa70ce06b
Belén M <100541119+bemartin94@users.noreply.github.com>
2026-06-08 15:36:00 -0300

    fix: aumentar max_length de campos de texto para compatibilidad con PostgreSQL

--- djangoback/tp3_crud/apps/juego/models.py
+++ djangoback/tp3_crud/apps/juego/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Juego(models.Model):
     Rank = models.IntegerField(null=True, blank= True)
-    Name = models.CharField(max_length=100, null=True, blank= True)
-    Platform = models.CharField(max_length=50, null=True, blank= True)
+    Name = models.CharField(max_length=255, null=True, blank= True)
+    Platform = models.CharField(max_length=100, null=True, blank= True)
     Year = models.IntegerField(null=True, blank= True)
-    Genre = models.CharField(max_length=100, null=True, blank= True)
-    Publisher = models.CharField(max_length=100, null=True, blank= True)
+    Genre = models.CharField(max_length=255, null=True, blank= True)
+    Publisher = models.CharField(max_length=255, null=True, blank= True)
     NA_Sales = models.FloatField(null=True, blank=True)
     EU_Sales = models.FloatField(null=True, blank=True)
     JP_Sales = models.FloatField(null=True, blank=True)
